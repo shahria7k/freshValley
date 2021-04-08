5f3b7662d55e83102cd7419d94f48e47be5a9f1d
Shahria Jaman Khan <41187226+shahria7k@users.noreply.github.com>
2021-04-08 22:56:11 +0600

    half done

---
+++ backend/data/products.js
@@ -0,0 +1,244 @@
+const products = [
+	{
+		name: "Fresh FUll cream milk",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2032.png",
+		description: "Grocery",
+		price: 200,
+		countInStock: 15,
+	},
+	{
+		name: "Spicy Leavs",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2033.png",
+		description: "Grocery",
+		price: 99,
+		countInStock: 10,
+	},
+	{
+		name: "Onion",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2034.png",
+		description: "Onion is on fire",
+		price: 130,
+		countInStock: 0,
+	},
+	{
+		name: "Miniket rich",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2035.png",
+		description: "Rice is what we love",
+		price: 50,
+		countInStock: 25,
+	},
+	{
+		name: "Gas",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2036.png",
+		description: "Something we love to give people",
+		price: 1233,
+		countInStock: 4,
+	},
+	{
+		name: "Mr twist",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2037.png",
+		description: "Firebase and mongodb gave me twist at the end",
+		price: 15,
+		countInStock: 10,
+	},
+	{
+		name: "Life boy soap",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2038.png",
+		description: "Its corona time. dont forget to clean your hands every while",
+		price: 130,
+		countInStock: 0,
+	},
+	{
+		name: "Ruchi Canachur",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2039.png",
+		description: "Ruchi chanachur is very testy",
+		price: 50,
+		countInStock: 25,
+	},
+	{
+		name: "Gas",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2036.png",
+		description: "Something we love to give people",
+		price: 1233,
+		countInStock: 4,
+	},
+	{
+		name: "Parasuit heir oil",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2040.png",
+		description: "Oil your own mechine",
+		price: 15,
+		countInStock: 10,
+	},
+	{
+		name: "Fresh FUll cream milk",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2032.png",
+		description: "Grocery",
+		price: 200,
+		countInStock: 15,
+	},
+	{
+		name: "Spicy Leavs",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2033.png",
+		description: "Grocery",
+		price: 99,
+		countInStock: 10,
+	},
+	{
+		name: "Onion",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2034.png",
+		description: "Onion is on fire",
+		price: 130,
+		countInStock: 0,
+	},
+	{
+		name: "Miniket rich",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2035.png",
+		description: "Rice is what we love",
+		price: 50,
+		countInStock: 25,
+	},
+	{
+		name: "Gas",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2036.png",
+		description: "Something we love to give people",
+		price: 1233,
+		countInStock: 4,
+	},
+	{
+		name: "Mr twist",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2037.png",
+		description: "Firebase and mongodb gave me twist at the end",
+		price: 15,
+		countInStock: 10,
+	},
+	{
+		name: "Life boy soap",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2038.png",
+		description: "Its corona time. dont forget to clean your hands every while",
+		price: 130,
+		countInStock: 0,
+	},
+	{
+		name: "Ruchi Canachur",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2039.png",
+		description: "Ruchi chanachur is very testy",
+		price: 50,
+		countInStock: 25,
+	},
+	{
+		name: "Gas",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2036.png",
+		description: "Something we love to give people",
+		price: 1233,
+		countInStock: 4,
+	},
+	{
+		name: "Parasuit heir oil",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2040.png",
+		description: "Oil your own mechine",
+		price: 15,
+		countInStock: 10,
+	},
+	{
+		name: "Fresh FUll cream milk",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2032.png",
+		description: "Grocery",
+		price: 200,
+		countInStock: 15,
+	},
+	{
+		name: "Spicy Leavs",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2033.png",
+		description: "Grocery",
+		price: 99,
+		countInStock: 10,
+	},
+	{
+		name: "Onion",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2034.png",
+		description: "Onion is on fire",
+		price: 130,
+		countInStock: 0,
+	},
+	{
+		name: "Miniket rich",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2035.png",
+		description: "Rice is what we love",
+		price: 50,
+		countInStock: 25,
+	},
+	{
+		name: "Gas",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2036.png",
+		description: "Something we love to give people",
+		price: 1233,
+		countInStock: 4,
+	},
+	{
+		name: "Mr twist",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2037.png",
+		description: "Firebase and mongodb gave me twist at the end",
+		price: 15,
+		countInStock: 10,
+	},
+	{
+		name: "Life boy soap",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2038.png",
+		description: "Its corona time. dont forget to clean your hands every while",
+		price: 130,
+		countInStock: 0,
+	},
+	{
+		name: "Ruchi Canachur",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2039.png",
+		description: "Ruchi chanachur is very testy",
+		price: 50,
+		countInStock: 25,
+	},
+	{
+		name: "Gas",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2036.png",
+		description: "Something we love to give people",
+		price: 1233,
+		countInStock: 4,
+	},
+	{
+		name: "Parasuit heir oil",
+		imageUrl:
+			"https://raw.githubusercontent.com/ProgrammingHero1/fresh-valley/main/Products/image%2040.png",
+		description: "Oil your own mechine",
+		price: 15,
+		countInStock: 10,
+	},
+];
+
+module.exports = products;
